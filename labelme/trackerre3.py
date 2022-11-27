347ec3da6dfc0f4b13b0568e18cbdea8257111ac
Eric Price <eric.price@tuebingen.mpg.de>
2022-11-27 12:51:30 +0100

    switched network to 32bit for all platforms

--- labelme/trackerre3.py
+++ labelme/trackerre3.py
@@ -37,7 +37,7 @@ class SSD():
 
     def __init__(self):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.precision=torch.float16 if torch.cuda.is_available() else torch.float32
+        self.precision=torch.float32 #16 if torch.cuda.is_available() else torch.float32
         self.model = model.SSD300(backbone=model.ResNet("resnet34"))
         self.model = self.model.to(self.precision)
 
@@ -51,7 +51,7 @@ class SSD():
         if self.device.type == "cpu":
             weights=torch.load("ssd.pt", map_location=lambda storage, loc: storage)["model"]
         else:
-            self.model=batchnorm_to_float(self.model)
+            #self.model=batchnorm_to_float(self.model)
             weights=torch.load("ssd.pt")["model"]
         self.model.load_state_dict(weights)
         self.model = self.model.to(self.device)
@@ -307,11 +307,11 @@ class SSD():
                 boxes = torch.cat((frameboxes[indices],self.hardZero.to(self.precision).view(1,1).expand(1,4)),0)
 
                 # do non maximum suppression
-                if self.device.type=="cpu":
-                    # likely a laptop, we need to limit memory usage
-                    index = scores.argsort(descending=True)[0:5000] # only the 5000 highest scores per class are kept, speeds things up in a worst case scenario
-                    scores=scores[index].contiguous()
-                    boxes=boxes[index].contiguous()
+                #if self.device.type=="cpu":
+                # likely a laptop, we need to limit memory usage
+                index = scores.argsort(descending=True)[0:5000] # only the 5000 highest scores per class are kept, speeds things up in a worst case scenario
+                scores=scores[index].contiguous()
+                boxes=boxes[index].contiguous()
                 overlapping = (ssdutils.calc_iou_tensor(boxes,boxes) > nms_iou) # 2x2 boolean matrix of all boxes with too high jaccard overlap - each row has at least one True value on diagonal
                 scorematrix = overlapping.float() * scores[:,None]       #this replaces the boolean values with the scores of the column-box
 
